927dc7f2d5c96c7f55ecfa67a91b6ef91cbeeee4
WilberJose <wilberjose200208@gmail.com>
2021-08-20 18:33:34 -0600

    biblioteca

---
+++ UsoBiblioteca/UsoBiblioteca/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace UsoBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione el elemento a crear: ");
+            Console.WriteLine("1. List");
+            Console.WriteLine("2. SortedList");
+            var response = Console.ReadLine();
+
+            //evaluar respuesta del usuario 
+            if (response =="1")
+            {
+                CollectionsExample.ListCollection();
+
+            }
+            else if(response=="2")
+            {
+                CollectionsExample.SortedListCollection();
+            }
+            Console.ReadKey();
+}
+    }
+}
+
